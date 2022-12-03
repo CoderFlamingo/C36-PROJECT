1bf88c163120b759ff774bcc43e9a2e7a6c3d312
CoderFlamingo <101107754+CoderFlamingo@users.noreply.github.com>
2022-12-03 11:58:58 +0530

    Update Question.js

--- js/Question.js
+++ js/Question.js
@@ -1,64 +1,67 @@
-class Question {
+class Quiz {
+  constructor(){}
 
-  constructor() {
-    this.title = createElement('h1')
-    this.input1 = createInput("Enter Your Name Here....");
-    this.input2 = createInput("Enter Correct Option No..");
-    this.button = createButton('Submit');
-    this.question = createElement('h3');
-    this.option1 = createElement('h4');
-    this.option2 = createElement('h4');
-    this.option3 = createElement('h4');
-    this.option4 = createElement('h4');
-    this.message = createElement("h2")
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
   }
 
-  hide(){
-    this.title.hide();
-    this.input1.hide();
-    this.button.hide();
-    this.input2.hide();
-    this.message.hide();
-  }
-
-  display(){
-    this.title.html("MyQuiz Game");
-    this.title.position(350, 0);
-
-    // this.question("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
-    // this.html("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
-    this.question.html("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
-    // question.html("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
-
-    this.question.position(150, 80);
-    this.option1.html("1: Everyone " );
-    this.option1.position(150, 100);
-    this.option2.html("2: Envelope" );
-    this.option2.position(150, 120);
-    this.option3.html("3: Estimate " );
-    this.option3.position(150, 140);
-    this.option4.html("4: Example" );
-    this.option4.position(150, 160);
-
-    this.input1.position(150, 230);
-    this.input2.position(350, 230);
-    this.button.position(290, 300);
-
-    this.button.mousePressed(()=>{
-      
-       this.message.html("Thank You, Your Answer Has Been Submitted");
-       this.message.position(350, 350);
-
-      // this.message.html("Thank You, Your Answer Has Been Submitted");
-      // this.message.position();
-
-      // this.message("Thank You, Your Answer Has Been Submitted");
-      // this.message(350, 350);
-
-      // this.message.html("Thank You, Your Answer Has Been Submitted");
-      // this.message.position(350);
-
-
+  update(state){
+    database.ref('/').update({
+      gameState: state
     });
   }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+    
+    question.hide(); 
+
+    background("Yellow");
+    fill(0);
+    textSize(30);
+    text("Result of the Quiz",340, 50);
+    text("----------------------------",320, 65);
+    Contestant.getPlayerInfo();
+    if(allContestants !== undefined){
+      debugger;
+      var display_Answers = 230;
+      fill("Blue");
+      textSize(20);
+      text("*NOTE: Contestant who answered correct are highlighted in green color!",130,230);
+
+      for(var plr in allContestants){
+        debugger;
+        var correctAns = "2";
+
+        if (correctAns === allContestants[plr].answer){
+          fill("Green")
+        }
+        else{
+          fill("red");
+        }
+
+
+        display_Answers+=30;
+        textSize(20);
+        text(allContestants[plr].name + ": " + allContestants[plr].answer, 250,display_Answers)
+      }
+    }
+  }
 }
+
